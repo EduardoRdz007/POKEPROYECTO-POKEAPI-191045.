4ad5be32da3292c7582d2eb46e65c7323503d1a9
EduardoRdz007 <edurdz010@gmail.com>
2022-07-05 12:09:59 -0500

    feat: A new component was added for the navigation of the pages, using constants

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -29,6 +29,34 @@ const Pokedex = (props) => {
         const nextPage = Math.max(total -1);
         setPage(nextPage);
     }
+    const Pokedex = (props) => {
+
+        const { pokemons, page, setPage, total, loading } = props;
+    
+        //retrocede una pagina
+        const lastPage = () => {
+            const nextPage = Math.max(page - 1, 0);
+            setPage(nextPage);
+        }
+    
+        //adelanta una pagina
+        const nextPage = () => {
+            const nextPage = Math.min(page + 1, total - 1);
+            setPage(nextPage);
+        }
+    
+        //envia a pagina 1
+        const firstPage = () => {
+            const nextPage = Math.min(page + total, total - total);
+            setPage(nextPage);
+        }
+    
+        //envia a la ultima pagina
+        const finalPage = () => {
+            const nextPage = Math.max(total -1);
+            setPage(nextPage);
+        }
+    
 
     return (
         <>
