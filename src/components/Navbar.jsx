5c3506e6d641fa858191a84ee90fff7384de2912
EduardoRdz007 <edurdz010@gmail.com>
2022-07-05 10:14:33 -0500

    feat: A navbar component was created to use the UseContext

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,4 +30,32 @@ const Navbar = () => {
     )
 }
 
+nst { useContext } = React;
+
+const Navbar = () => {
+
+    const {favoritePokemons} = useContext(FavoriteContext);
+    
+    const pokeLogo = "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+    
+    return (
+        <>
+          <div>
+          <nav>
+              <div>
+              <img src={pokeLogo} 
+              alt="pokemon-logo"
+              className="navbar-image"
+              />
+              </div>
+              <div className="navbar-heart">
+              &#10084;&#65039; {favoritePokemons.length}
+              </div>
+          </nav>
+          </div>
+            
+        </>
+    )
+}
+
 export default Navbar
