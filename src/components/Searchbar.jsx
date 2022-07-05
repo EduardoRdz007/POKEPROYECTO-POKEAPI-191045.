8bae4b4a6e11f871da9dc3395366f42467ca9b88
EduardoRdz007 <edurdz010@gmail.com>
2022-07-05 10:38:32 -0500

    style: Added styles for the Searchbar component

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -35,38 +35,5 @@ const Searchbar = (props) => {
         </>
     )
 }
-const Searchbar = (props) => {
-    const { onSearch } = props;
-    const [search, setSearch] = useState("");
 
-    const onChange = (e) => {
-        setSearch(e.target.value);
-        if (e.target.value.length === 0) {
-            onSearch(null);
-        }
-    }
-
-    const onClick = async (e) => {
-        onSearch(search);
-    }
-
-    return (
-        <>
-          <div className="searchbar-container">
-              <div className="searchbar">
-                 <input 
-                 type="text"
-                 placeholder="Buscar Pokemon"
-                 onChange={onChange} 
-                 />
-              </div>
-              <div className="searchbar-btn">
-                 <button onClick={onClick}>
-                     Buscar
-                 </button>
-              </div>
-          </div>  
-        </>
-    )
-}
 export default Searchbar
