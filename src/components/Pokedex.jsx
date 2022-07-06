462ff30f1043febde442a38f34f4496632e7bfbf
EduardoRdz007 <edurdz010@gmail.com>
2022-07-06 16:35:17 -0500

    The bottles were repositioned to give next to the pokemon and the cards. Then visualization tests were done

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -35,7 +35,6 @@ const Pokedex = (props) => {
         <>
         <div>
             <div className="header">
-                <h1>Pokemons</h1>
                 <Pagination 
                    page={page + 1}
                    totalPages={total} 
